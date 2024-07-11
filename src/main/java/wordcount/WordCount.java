9e6676f37d5f86b13ab9d347369a0985a65916b3
13574798@qq.com <13574798@qq.com>
2024-07-11 22:46:42 +0800

    ll

--- src/main/java/wordcount/WordCount.java
+++ src/main/java/wordcount/WordCount.java
@@ -1,18 +1,48 @@
 package wordcount;
 
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.runtime.execution.Environment;
 import org.apache.flink.streaming.api.scala.DataStream;
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
 
 public class WordCount {
 
     public static void main(String[] args) {
 
-        StreamExecutionEnvironment env  = StreamExecutionEnvironment.getExecutionEnvironment();
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(2);
+        DataSet<String> text = env.readTextFile("/path/to/file");
 
-      DataStream<String> dataStream =  env.addSource();
+        DataSet<Tuple2<String, Integer>> counts =
+                // split up the lines in pairs (2-tuples) containing: (word,1)
+                text.flatMap(new Tokenizer())
+                        // group by the tuple field "0" and sum up tuple field "1"
+                        .groupBy(0)
+                        .sum(1);
 
-        dataStream.flatMap().keyBy(0).sum(1).print();
+        counts.writeAsCsv("outputPath", "\n", " ");
+
+// User-defined functions
+
+    }
+    static class Tokenizer implements FlatMapFunction<String, Tuple2<String, Integer>> {
+
+        @Override
+        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+            // normalize and split the line
+            String[] tokens = value.toLowerCase().split("\\W+");
+
+            // emit the pairs
+            for (String token : tokens) {
+                if (token.length() > 0) {
+                    out.collect(new Tuple2<String, Integer>(token, 1));
+                }
+            }
+        }
     }
 
 }
