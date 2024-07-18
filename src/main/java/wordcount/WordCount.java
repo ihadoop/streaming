0ca856288f98c477f52505dc63fc3be058f921f6
13574798@qq.com <13574798@qq.com>
2024-07-18 22:48:08 +0800

    ll

--- src/main/java/wordcount/WordCount.java
+++ src/main/java/wordcount/WordCount.java
@@ -1,24 +1,70 @@
 package wordcount;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.configuration.RestOptions;
 import org.apache.flink.runtime.execution.Environment;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.scala.DataStream;
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.util.Collector;
+import scala.collection.Seq;
 
 public class WordCount {
+    public static class WordWithCount {
 
+        public String word;
+        public long count;
+
+        public WordWithCount() {}
+
+        public WordWithCount(String word, long count) {
+            this.word = word;
+            this.count = count;
+        }
+
+        @Override
+        public String toString() {
+            return word + ": " + count;
+        }
+    }
     public static void main(String[] args) {
 
         Configuration configuration = new Configuration();
         configuration.setInteger(RestOptions.PORT,8082);
         StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(configuration);
 
+        // 3. 初始化数据
+        DataStream<String> source = env.socketTextStream("127.0.0.1", 9999,' ',10000L);
+        source.flatMap(new FlatMapFunction<String, WordWithCount>() {
+            @Override
+            public void flatMap(String s, Collector<WordWithCount> collector) throws Exception {
+                String[] words = s.split("\\s+");
+                for(String word : words) {
+                    collector.collect(new WordWithCount(word, 1L));
+                }
+                //collector.collect(new WordWithCount());
+            }
+        }, TypeInformation.of(WordWithCount.class)).keyBy(new KeySelector<WordWithCount, String>() {
+            @Override
+            public String getKey(WordWithCount wordWithCount) throws Exception {
+                return wordWithCount.word;
+            }
+        },TypeInformation.of(String.class)).sum("count").print();
+
+        // 5. 打印结果,设置并行度
+       // counts.print().setParallelism(1);
+
+        // 6. 开启流任务,这是一个action算子,将触发计算
+        env.execute("SocketWindowWordCountJava");
 
 // User-defined functions
 
