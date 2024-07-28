7d6b289739a377cbc0a68c751d7d6d74bcbd5510
13574798@qq.com <13574798@qq.com>
2024-07-28 23:29:25 +0800

    ll

---
+++ src/main/java/stock/StockMain.java
@@ -0,0 +1,16 @@
+package stock;
+
+import org.apache.flink.streaming.api.datastream.DataStream;
+
+public class StockMain {
+    public static void main(String[] args) {
+
+        DataStream<StockPrice> dataStream = null;
+
+
+
+
+
+
+    }
+}
