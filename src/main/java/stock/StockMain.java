e4a31865ec00a26448a840ead3fcc17dcea0eea9
13574798@qq.com <13574798@qq.com>
2024-07-31 21:41:11 +0800

    ll

--- src/main/java/stock/StockMain.java
+++ src/main/java/stock/StockMain.java
@@ -1,5 +1,7 @@
 package stock;
 
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.configuration.RestOptions;
@@ -19,16 +21,27 @@ public class StockMain {
 
 
 
-        dataStream.keyBy(new KeySelector<StockPrice, String>() {
+        DataStream<StockPrice> maxDataStream  = dataStream.map(new MapFunction<StockPrice, StockPrice>() {
+            @Override
+            public StockPrice map(StockPrice value) throws Exception {
+                value.setPrice(value.getPrice()*7);
+                return value;
+            }
+        }).keyBy(new KeySelector<StockPrice, String>() {
 
             @Override
             public String getKey(StockPrice value) throws Exception {
                 return value.getCode();
             }
-        }).min("price").print();
+        }).min("price");
 
 
-
+        DataStream<StockPrice> filterDataStream  = dataStream.filter(new FilterFunction<StockPrice>() {
+            @Override
+            public boolean filter(StockPrice value) throws Exception {
+                return false;
+            }
+        });
 
 
         env.execute("SocketWindowWordCountJava");
